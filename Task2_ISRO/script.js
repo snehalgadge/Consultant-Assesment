d1ba4c52fea9b5aadfab07a7ddecb954026e3c1e
Snehal <snehalgadge205@gmail.com>
2023-04-06 16:53:37 +0530

    Task 2 in process

--- Task2_ISRO/script.js
+++ Task2_ISRO/script.js
@@ -1,26 +1,28 @@
 function loadSpacecrafts() {
-    fetch('https://isro.vercel.app/api/spacecrafts')
-      .then(res => res.json())
-      .then(data => {
-
-        data.forEach(user => {
-          const markup = `<li>${user.name}</li>`;
-          document.querySelector('.spaceCraft').insertAdjacentHTML('beforeend', markup);
-        })
-
-      })
-      .catch(error => console.log(error));
-
-      fetch('https://isro.vercel.app/api/launchers')
-      .then(res => res.json())
-      .then(data => {
-
-        data.forEach(user => {
-          const markup = `<li>${user.name}</li>`;
-          document.querySelector('.launches').insertAdjacentHTML('beforeend', markup);
-        })
-      })
-      .catch(error => console.log(error)); 
+    const api1 = 'https://isro.vercel.app/api/spacecrafts';
+    const api2 = 'https://isro.vercel.app/api/launchers';
+    const api3 = 'https://isro.vercel.app/api/customer_satellites';
+    const api4 = 'https://isro.vercel.app/api/centres'
+    
+    Promise.all([
+      fetch(api1),
+      fetch(api2),
+      fetch(api3),
+      fetch(api4)
+    ])
+    .then(responses => {
+      return Promise.all(responses.map(response => {
+        return response.json();
+      }));
+    })
+    .then(data => {
+      // process data from each API
+      const data1 = data[0];
+      const data2 = data[1];
+      const data3 = data[2];
+      console.log(data1, data2, data3);
+    })
+    .catch(error => console.log(error)); 
   }
   
   loadSpacecrafts();
